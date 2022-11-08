b8d82b62c823c200916f38aa96846e17d27d94be
Владислав <rybvlad4@yandex.ru>
2022-11-08 16:35:19 +0300

    better buttons

--- HUD/StartScreen/MenuButton.gd
+++ HUD/StartScreen/MenuButton.gd
@@ -1,22 +1,35 @@
-extends ColorRect
+extends Panel
 
 export var title = "title_"
 export var menu_num = 0
+var new_style
 
 func _ready():
+	new_style = StyleBoxFlat.new()
+	new_style.set_corner_radius_all(10)
+	new_style.set_bg_color(Color("#c6251e1e"))
+	self.set('custom_styles/panel', new_style)
 	$Label.text = title
+	
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
-		self.on_click()
+		self._on_click()
 
-func on_click():
+func _on_click():
 	match menu_num:
 		0:
 			get_tree().change_scene("res://main.tscn")
 		1:
-			print("clown")
+			get_tree().change_scene("res://HUD/SettingsScreen/SettingsScreen.tscn")
 		2:
 			get_tree().quit()
+
+
+func _on_Area2D_mouse_entered():
+	new_style.set_bg_color(Color("#a0986969"))
+
+func _on_Area2D_mouse_exited():
+	new_style.set_bg_color(Color("#c6251e1e"))
