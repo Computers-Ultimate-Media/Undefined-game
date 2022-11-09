3f75519c6c37dd06c597abdf169e4abc5866bfbf
Roman <roman.qwonix@gmail.com>
2022-11-10 00:18:09 +0300

    moving main.gd func to Inventory.gd

---
+++ Inventory/Inventory.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+var bodies = []
+var heads = []
+var feet = []
+
+
+func _ready():
+	var dict_heads = read_from_JSON("res://Assets/JSON/Player/Head/player_heads.json")
+	for key in dict_heads.keys():
+		var head = load("res://Player/Head.tscn").instance()
+		head.texture = load(dict_heads[key]["texture"])
+		head.hpMax = dict_heads[key]["hpMax"]
+
+		head.name = key
+
+		heads.append(head)
+
+	var dict_bodies = read_from_JSON("res://Assets/JSON/Player/Body/player_bodies.json")
+	for key in dict_bodies.keys():
+		var body = load("res://Player/Body.tscn").instance()
+		body.texture = load(dict_bodies[key]["texture"])
+		body.hpBody = dict_bodies[key]["hpBody"]
+		body.hpRegen = dict_bodies[key]["hpRegen"]
+
+		body.name = key
+
+		bodies.append(body)
+
+
+	var dict_feet = read_from_JSON("res://Assets/JSON/Player/Foot/player_feet.json")
+	for key in dict_feet.keys():
+		var foot = load("res://Player/Foot.tscn").instance()
+		foot.texture = load(dict_feet[key]["texture"])
+		foot.moveSpeed = dict_feet[key]["moveSpeed"]
+
+		foot.name = key
+
+		feet.append(foot)
+
+func read_from_JSON(path):
+	var file = File.new()
+	if file.file_exists(path):
+		file.open(path, File.READ)
+		var data = parse_json(file.get_as_text())
+		file.close()
+		return data
+	else:
+		printerr("Invalid path given")
