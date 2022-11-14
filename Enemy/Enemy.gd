fb19b767e5dac5459a31d6c5380b4425c4f43b3c
Владислав <rybvlad4@yandex.ru>
2022-11-14 23:42:43 +0300

    better enemy behavior

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -5,65 +5,66 @@ class_name Enemy
 var player = null
 var target = null
 
+onready var floating_text = preload("res://HUD/InGameHUD/FloatingText.tscn") 
+
 onready var projectile 
 onready var weapon = $Blade
+onready var health_bar = $HeathBar
 
 var velocity = Vector2.ZERO
 var last_direction = Vector2(0, 1)
-var bounce_countdown = 0
+var shooting_time
+var shooting_distance
 
-export var health = 100
-export var max_health = 100
-export var damage = 10
-export var speed = 1000
-
-signal enemy_dead;
-signal enemy_hit;
-signal enemy_shoot;
-
-onready var health_bar = $HeathBar
+export var health : int
+export var max_health : int
+export var speed : int
 
 func _ready():
+	shooting_time = weapon.get_reload_time()
+	shooting_distance = weapon.get_shooting_distance()
 	player = get_tree().root.get_node("main/Player")
 	health_bar._on_max_health_updated(max_health)
-	
-func _physics_process(delta):
+
+func _process(delta):
 	velocity = Vector2.ZERO
-	if target != null:
-		velocity = position.direction_to(target.position) * speed
+	
+	if (self.global_position.distance_to(player.global_position)) < 350:
+		target = player
+		
+		velocity = self.global_position.direction_to(target.global_position) * speed
+		if (self.global_position.distance_to(player.global_position)) < shooting_distance:
+			velocity = Vector2.ZERO
+			
+		if($ShootTimer.is_stopped()):
+			emit_signal("enemy_shoot")
+			$ShootTimer.start()
 	else:
 		velocity = Vector2.ZERO
+		target = null
+		$ShootTimer.stop()
+		
 	velocity = velocity.normalized()
 	velocity = move_and_collide(velocity)
 
 func hit(damage):
+	var text = floating_text.instance()
+	text.amount = damage
+	add_child(text)
+	
 	health -= damage
-	emit_signal("enemy_hit")
+	health_bar._on_health_updated(health, 0)
 	health_bar._display_health_bar(true)
 
 func death():
 	self.queue_free()
-	emit_signal("enemy_dead")
-
-func _on_Enemy_enemy_dead():
 	player.setCoins(player.getCoins() + 10)
 
-func _on_Enemy_enemy_hit():
-	health_bar._on_health_updated(health, 0)
-
-func _on_PlayerFindArea_body_entered(body):
-	if body.name == "Player":
-		target = body
-		$ShootTimer.start()
-
-func _on_PlayerFindArea_body_exited(body):
-	target = null
-
 func _on_ShootTimer_timeout():
-	emit_signal("enemy_shoot")
+	enemy_shoot()
 
-func _on_Enemy_enemy_shoot():
+func enemy_shoot():
 	if target != null:
 		weapon.target = target
 		weapon.shoot()
-		$ShootTimer.wait_time = 1
+		$ShootTimer.wait_time = shooting_time
