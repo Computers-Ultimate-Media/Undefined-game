78fbd499d1e7d6f14e183b0454ae662dad3158d5
Владислав <rybvlad4@yandex.ru>
2022-11-14 23:46:35 +0300

    better shooting

--- Weapons/Projectile.gd
+++ Weapons/Projectile.gd
@@ -3,23 +3,19 @@ extends Node2D
 class_name Projectile
 
 var weapon_owner setget set_weapon_owner
-var max_damage: int
-var min_damage: int
-
-export var critical_damage: int
-export var critical_damage_chance: int
-export var max_distance_shot: int = 50
-export var speed: int = 5
-
 var direction := Vector2(-1, 0) setget set_direction
+
+var damage: int
+var time: int
+var speed: int
+
 onready var timer = $KillTimer
 
 func _ready():
-	timer.start(max_distance_shot / speed * 3)
+	timer.start(time / 3)
 	
 func attack(target):
-	var current_dmg = round(rand_range(min_damage, max_damage))
-	target.hit(current_dmg)
+	target.hit(damage)
 	queue_free()
 	if(target.health <= 0):
 		target.death()
--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -8,6 +8,13 @@ export var weapon_owner = "title_"
 export var max_damage = 10
 export var min_damage = 5
 
+export var critical_damage: int
+export var critical_damage_chance: int
+export var max_distance_shot: int
+export var speed: int
+
+export var reload_time: int
+
 func shoot():
 	if weapon_owner == "Player":
 		target_position = get_global_mouse_position()
@@ -20,8 +27,23 @@ func shoot():
 	
 	projectile.global_position = $ShootPoint.global_position
 	projectile.set_weapon_owner(weapon_owner)
-	projectile.max_damage = max_damage
-	projectile.min_damage = min_damage
 	projectile.set_direction(direction) 
 	
+	projectile.damage = calculate_damage()
+	projectile.time = max_distance_shot / speed
+	projectile.speed = speed
+	
 	get_tree().root.get_child(0).add_child(projectile)
+
+func calculate_damage():
+	var damage = round(rand_range(min_damage, max_damage))
+	if(rand_range(0, 100) < critical_damage_chance):
+		damage = damage * (critical_damage / 100)
+		print("crit!")
+	return damage
+
+func get_reload_time():
+	return reload_time
+
+func get_shooting_distance():
+	return max_distance_shot
