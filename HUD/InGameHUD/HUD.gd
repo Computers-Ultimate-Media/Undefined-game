251ea6b80a0fec6761770b7f337d5a69c55334c7
Roman <roman.qwonix@gmail.com>
2022-12-16 12:53:52 +0300

    add regen and move speed value on hud

--- HUD/InGameHUD/HUD.gd
+++ HUD/InGameHUD/HUD.gd
@@ -1,14 +1,18 @@
 extends CanvasLayer
 
 func _on_Player_player_stats_changed(player):
-	$HP/Label.text = str(player.getHpCurrent()) + "/" + str(player.getHpMax())
-	$Armor/Label.text = str(player.getArmorCurrent()) + "/" + str(player.getArmorMax())
-	$Coin/Label.text = str(player.getCoins())
+	$HP/Label.text = str(player.health) + "/" + str(player.hpMax)
+	$Armor/Label.text = str(player.armorCurrent) + "/" + str(player.armorMax)
+	$Coin/Label.text = str(player.coins)
+	$Regen/Label.text = str(player.hpRegen)
+	$MoveSpeed/Label.text = str(player.moveSpeed)
+
 	
 func _on_Player_player_dead():
 	get_tree().paused = true
 	$GameOver.visible = true
-	
 	$HP.visible = false
 	$Armor.visible = false
 	$Coin.visible = false
+	$Regen.visible= false
+	$MoveSpeed.visible = false
