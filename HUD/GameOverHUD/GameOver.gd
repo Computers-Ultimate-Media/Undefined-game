e336b78ca2b0fa5c7be5a5597325cc008ad06f09
Roman <roman.qwonix@gmail.com>
2023-02-03 10:56:47 +0300

    last changes

--- HUD/GameOverHUD/GameOver.gd
+++ HUD/GameOverHUD/GameOver.gd
@@ -7,3 +7,4 @@ func _on_Player_player_dead():
 	get_tree().root.get_node("main/HUD/Armor").visible = false
 	get_tree().root.get_node("main/HUD/Coin").visible = false
 	get_tree().root.get_node("main/Inventory").visible = false
+	get_tree().root.get_node("main/SkilsMenu").visible = false
